036a7063bfbb6bbc17743b1c527e49fe8e7349d8
DevMofu <DevMoFu>
2020-05-18 13:53:29 -0400

    adding win check algos

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 var gameBoard [3][3]string
+var winConditions = [2]string{"xxx", "ooo"}
 
 func viewBoard(b [3][3]string) {
 	for i := range gameBoard {
@@ -39,9 +40,48 @@ func fillPosition(user string, position uint) {
 	fmt.Println(b)
 }
 
-//func winDector([3][3][3]string) bool {
-//	nil
-//}
+//// Check board conditions
+// TODO: Fix return
+func checkRows(b [3][3]string) string {
+	for i := range b {
+		rowResult := fmt.Sprintf("%v%v%v", b[i], b[i+1], b[i+2])
+		if rowResult == winConditions[0] || rowResult == winConditions[1] {
+			return "win"
+		}
+	}
+}
+
+// TODO: Fix return
+func checkColumns(b [3][3]string) string {
+	for i := range b {
+		columnResult := fmt.Sprintf("%v%v%v", b[0][i], b[1][i], b[2][i])
+		if columnResult == winConditions[0] || columnResult == winConditions[1] {
+			return "win"
+		}
+	}
+}
+
+// fix return
+func checkDiagonal(b [3][3]string) string {
+	i, j, k := 0, 1, 2
+
+	diagonalRight := fmt.Sprintf("%v%v%v", b[0][i], b[j][j], b[2][k])
+	diagonalLeft := fmt.Sprintf("%v%v%v", b[0][k], b[j][j], b[2][i])
+	allDiagonalResults := [2]string{diagonalLeft, diagonalRight}
+
+	for l := range allDiagonalResults {
+		for m := range winConditions {
+			if allDiagonalResults[i] == winConditions[m] {
+				return "win"
+			}
+		}
+	}
+}
+
+func gameProgress(b [3][3]string) string {
+	// 3 across, 3 down, 3 diagonal
+
+}
 
 func main() {
 	viewBoard(gameBoard)
